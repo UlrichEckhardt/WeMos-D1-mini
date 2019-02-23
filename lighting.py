78768c197538c6cb2b640fefe4f048067064b384
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2019-02-23 23:23:17 +0100

    lighting.py: Refactoring, extract the animation itself to a separate class.

--- lighting.py
+++ lighting.py
@@ -6,29 +6,31 @@ from kivy.uix.widget import Widget
 import math
 tau = math.pi * 2
 
-WHITE = (1, 1, 1)
-
 
 class LightingWidget(Widget):
-    def __init__(self, **kwargs):
+    """simple widget to illustrate the lighting pattern
 
-        if 'led_count' in kwargs:
-            self._led_count = kwargs.pop('led_count')
-        else:
-            self._led_count = 10
+    This is used to develop the pattern that is displayed on an LED ring without
+    requiring access to actual hardware. The pattern is supplied with a plugin.
+    It also defines the number of LEDs that should be visualized.
 
+    """
+
+    def __init__(self, animation, **kwargs):
         super().__init__(**kwargs)
 
+        self._animation = animation
+
     def prepare(self):
-        self._time = 0
         self._leds = []
 
         with self.canvas:
-            for i in range(self._led_count):
+            pattern = self._animation(0)
+            for i, c in enumerate(pattern):
                 # append the colors to our list of colors so we can adjust them lateron
-                self._leds.append(Color(*WHITE))
-                x = 100 + 100 * math.cos(i * tau / self._led_count)
-                y = 100 + 100 * math.sin(i * tau / self._led_count)
+                self._leds.append(Color(*c))
+                x = 200 + 100 * math.cos(i * tau / len(pattern))
+                y = 200 + 100 * math.sin(i * tau / len(pattern))
                 Ellipse(pos=(x, y), size=(10, 10))
 
         # We'll update our variables in a clock
@@ -36,11 +38,16 @@ class LightingWidget(Widget):
 
     def update_animation(self, delta):
         pattern = self._animation(delta)
+        for i, c in enumerate(pattern):
+            self._leds[i].rgb = c
 
-        for i in range(self._led_count):
-            self._leds[i].rgb = pattern[i]
 
-    def _animation(self, delta):
+class Animation:
+    def __init__(self, led_count):
+        self._time = 0
+        self._led_count = led_count
+
+    def generate(self, delta):
         self._time += delta
 
         res = []
@@ -56,7 +63,8 @@ class LightingWidget(Widget):
 
 class LightingApp(App):
     def build(self):
-        widget = LightingWidget(led_count=60)
+        animation = Animation(led_count=60)
+        widget = LightingWidget(animation=animation.generate)
         widget.prepare()
         return widget
 
