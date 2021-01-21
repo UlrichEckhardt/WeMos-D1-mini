91a9a0da82537af76d5678062ee9db3995b0e08d
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2021-01-21 22:03:31 +0100

    d1mini.py: Constants with Wemos D1-Mini pin numbers.

---
+++ d1mini.py
@@ -0,0 +1,11 @@
+from micropython import const
+
+PIN_D0 = const(16)
+PIN_D1 = const(5)
+PIN_D2 = const(4)
+PIN_D3 = const(0) # Pull Up
+PIN_D4 = const(2) # LED
+PIN_D5 = const(14)
+PIN_D6 = const(12)
+PIN_D7 = const(13)
+PIN_D8 = const(15) # Pull Down
