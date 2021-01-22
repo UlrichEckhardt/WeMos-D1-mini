ab3e0c43182fbad265c08ff68d8bef181c702d72
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2021-01-22 19:23:46 +0100

    mecanum-drift: inputs.py: Average over multiple samples to smooth out the curve a bit.

--- mecanum-drift-firmware/inputs.py
+++ mecanum-drift-firmware/inputs.py
@@ -2,6 +2,11 @@ import d1mini
 
 from time import ticks_us, ticks_diff
 from machine import Pin, disable_irq, enable_irq
+from micropython import const
+from array import array
+
+# number of samples to average over
+_QUEUE_SIZE = const(10)
 
 class DutyCycle:
     """duty-cycle measurement on GPIO inputs"""
@@ -9,20 +14,22 @@ class DutyCycle:
     def __init__(self, pin_nr):
         """initialize with the GPIO pin number"""
         self.high_state = ticks_us()
-        self.diff = ticks_diff(self.high_state, self.high_state)
+        self.samples = array('i', [0] * _QUEUE_SIZE)
+        self.idx = 0
         pin = Pin(pin_nr, Pin.IN)
         pin.irq(trigger=Pin.IRQ_RISING|Pin.IRQ_FALLING, handler=self._handler)
 
     def value(self):
         """retrieve the duty cycle in microseconds"""
         state = disable_irq()
-        res = self.diff
+        samples = self.samples[:]
         enable_irq(state)
-        return res
+        return (sum(samples) + _QUEUE_SIZE // 2) // _QUEUE_SIZE
 
     def _handler(self, pin):
         now = ticks_us()
         if pin.value():
             self.high_state = now
         else:
-            self.diff = ticks_diff(self.high_state, now)
+            self.samples[self.idx] = ticks_diff(self.high_state, now)
+            self.idx = (1 + self.idx) % _QUEUE_SIZE
