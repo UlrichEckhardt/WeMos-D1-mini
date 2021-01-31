93b7789c131aca1f86ac18e7eb50678a85f1df23
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2021-01-31 21:52:57 +0100

    inputs_demo: import pins from main.py

--- mecanum-drift-firmware/inputs_demo.py
+++ mecanum-drift-firmware/inputs_demo.py
@@ -6,21 +6,13 @@ be used to check this part of the code on its own.
 import d1mini
 import inputs
 from utime import sleep_ms
-
-# receiver channel 2
-PIN_NICK = d1mini.PIN_D3
-# receiver channel 4
-PIN_ROLL = d1mini.PIN_D1
-# receiver channel 1
-PIN_YAW = d1mini.PIN_D4
-# receiver channel 3
-PIN_PITCH = d1mini.PIN_D2
+from main import PIN_ROLL, PIN_NICK, PIN_YAW
 
 def run():
-    d = inputs.DutyCycle([PIN_ROLL, PIN_NICK, PIN_YAW, PIN_PITCH])
+    d = inputs.DutyCycle((PIN_ROLL, PIN_NICK, PIN_YAW,))
 
     while True:
-        print(list(d.value()))
+        print(d.value())
         sleep_ms(200)
 
 run()
